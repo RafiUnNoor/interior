b64873955f1a5de891eee38bdecc22dd0bd14fe6
RafiUnNoor <rafiun94@gmail.com>
2023-10-01 01:23:27 +0600

    jquery and button added

--- in-op.js
+++ in-op.js
@@ -1,31 +1,63 @@
 
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
+$(document).ready(function(){
+
+    $("#myRange").on("input", function(e) {
+        $("#demo").css("width", e.target.value + "px");
+    });
 
 
+    $("#myColor").on("input", function() {
+        var colorValue = $(this).val();
+        $("#demo").css("background-color", colorValue);
+    });
 
-slider.addEventListener('input', e=>{
-    output.style.width = e.target.value + 'px'
-});
-
-var color = document.getElementById("myColor");
-color.addEventListener("input" , ()=>{
-    output.style.backgroundColor = color.value
-});
+    $("#plantCanvas").on( "click", function( event ) {
+        event.preventDefault();
+        $( "#plantation").toggle();
+      });
 
 
-document.getElementById("lamp3").addEventListener("click",function(){
-   var lamp3 = document.getElementById("lampThree");
-   if(lamp3.style.display == 'none'){
-    lamp3.style.display = 'block';
-   }else{lamp3.style.display = 'none';}
-   event.preventDefault();  
-});
+//lamp selection
+    $( "#lamp3" ).on( "click", function( event ) {
+        event.preventDefault();
+        var lamp2 = $("#lampTwo");
+        var lamp3 = $("#lampThree");
+        var lamp1 = $("#lampOne");
+        
+        if (lamp2.css("display") === "none" && lamp1.css("display") === "none") {
+              lamp3.toggle() ;
+        } else {
+            alert("You can choose only one type of lamp");
+        }
+      });
 
-document.getElementById("plantCanvas").addEventListener("click",function(){
-   var plnt = document.getElementById("plantation");
-   if(plnt.style.display == 'none'){
-    plnt.style.display = 'block';
-   }else{plnt.style.display = 'none';}
-   event.preventDefault();  
+      $( "#lamp1" ).on( "click", function( event ) {
+        event.preventDefault();
+        var lamp2 = $("#lampTwo");
+        var lamp3 = $("#lampThree");
+        var lamp1 = $("#lampOne");
+        
+        if (lamp2.css("display") === "none" && lamp3.css("display") === "none") {
+              lamp1.toggle() ;
+        } else {
+            alert("You can choose only one type of lamp");
+        }
+      });
+
+      $("#lamp2").click(function(event) {
+        event.preventDefault();
+        var lamp2 = $("#lampTwo");
+        var lamp3 = $("#lampThree");
+        var lamp1 = $("#lampOne");
+        
+        if (lamp3.css("display") === "none" && lamp1.css("display") === "none") {
+              lamp2.toggle() ;
+        } else {
+            alert("You can choose only one type of lamp");
+        }
+//lamp selection ends
+
+//sofa selection starts
+
+    });
 });
