e5f1ea6ad60b6a752a3dec53ec972faa61db9884
RafiUnNoor <rafiun94@gmail.com>
2023-10-27 01:45:50 +0600

    modifed header and footer

--- in-op.js
+++ in-op.js
@@ -1,6 +1,39 @@
 
 $(document).ready(function(){
 
+  var doAnimations = function() {
+      
+    // Calc current offset and get all animatables
+    var offset = $(window).scrollTop() + $(window).height(),
+        $animatables = $('.animatable');
+    
+    // Unbind scroll handler if we have no animatables
+    if ($animatables.length == 0) {
+      $(window).off('scroll', doAnimations);
+    }
+    
+    // Check all animatables and animate them if necessary
+        $animatables.each(function(i) {
+       var $animatable = $(this);
+            if (($animatable.offset().top + $animatable.height() - 40) < offset) {
+        $animatable.removeClass('animatable').addClass('animated');
+            }
+    });
+
+    };
+  
+  // Hook doAnimations on scroll, and trigger a scroll
+    $(window).on('scroll', doAnimations);
+  $(window).trigger('scroll');
+
+
+
+  $("#midScrool").click(function(){
+    $('html, body').scrollTop($("#designInterior").offset().top);
+ });
+
+
+
     $("#myRange").on("input", function(e) {
         $("#demo").css("width", e.target.value + "px");
     });
